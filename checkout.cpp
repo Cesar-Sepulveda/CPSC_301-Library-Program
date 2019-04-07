77f5e7ecf418b567b53ce6baec2345a4401cff5c
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-04-07 13:49:26 -0700

    Update checkout.cpp

--- checkout.cpp
+++ checkout.cpp
@@ -19,26 +19,63 @@ void printmenu() {
     cout << "8. Close program" << endl;
   }
 
-//------------------------------------------------------------------------------
 void readBookFile(){
 
 }
 
-//------------------------------------------------------------------------------
 void readPersonFile(){
 
 }
 
-//------------------------------------------------------------------------------
+void bookCheckout(vector<Book *> bookVec, vector<Person *> perVec, int ID){
+    int bookID;
+    int person = -1;
+    int book = -1;
+    for(int i; i < perVec.size(); ++i){
+        if(perVec.at(i)->getCardID() == ID){
+           person = i;
+        }
+    }
+    if(person  == -1){
+        cout << "Card ID not found" << endl;
+        return;
+    }
+    cout << "Cardholder: " << perVec.at(person)->getFirstName() << " " << perVec.at(person)->getLastName() << endl;
+    cout << "Please enter the book ID: ";
+    cin >> bookID;
+    for(int j; j < bookVec.size(); ++j){
+        if(bookVec.at(j)->getBookID() == bookID){
+           book = j;
+        }
+    }
+    if(book = -1){
+        cout << "Book ID not found" << endl;
+        return;
+    }
+    cout << "Title: " << bookVec.at(book)->getTitle() << endl;
+    if(bookVec.at(book)->getChecked() == false){
+        bookVec.at(book)->setChecked(true);
+        cout << "Rental Completed" << endl;
+    }else{
+        cout << "Book already checked out" << endl;
+    }
+}
+
+
+
 int main(){
   vector<Book *> books;
   vector<Person *> cardholders;
   int choice;
+  int cardID;
   do{
     printmenu();
     cin >> choice;
     switch(choice){
       case 1:
+        cout << "Please enter the card ID: ";
+        cin >> cardID;
+        bookCheckout(books, cardholders, cardID);
         break;
       case 2:
         break;
