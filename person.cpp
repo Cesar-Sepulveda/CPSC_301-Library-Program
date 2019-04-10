ef343e6868f2dcc69cd1524458d99843839e6e05
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-04-09 20:59:47 -0700

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,5 +1,8 @@
+//Cesar Sepulveda
 #include "person.h"
 #include <string>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -8,6 +11,12 @@ Person::Person(){
   active = false;
   firstName = " ";
   lastName = " ";
+}
+Person::Person(int id, bool act, string fn, string ln){
+  cardID = id;
+  active = act;
+  firstName = fn;
+  lastName = ln;
 }
 int Person::getCardID(){
   return cardID;
@@ -32,4 +41,24 @@ string Person::getLastName(){
 }
 void Person::setLastName(string lName){
   lastName = lName;
+}
+void Person::addBook(Book *b){
+    checked.push_back(b);
+}
+void Person::checkout(bool user){
+    if(checked.size() > 0){
+        for(int i = 0; i < checked.size(); ++i){
+            cout << "Book ID : " << checked.at(i)->getBookID() << endl;
+            cout << "Title: " << checked.at(i)->getTitle() << endl;
+            cout << "Author: " << checked.at(i)->getAuthor() << endl;
+            if(user == false){
+              cout << "Cardholder: " << getFirstName() << " " << getLastName();
+              cout << "Card ID: " << getCardID() << endl;
+            }
+        }
+    }
+}
+
+int Person::checkedSize(){
+    return checked.size();
 }
